f9e4503507dc78f9863635689d1217119221b7e9
FVitorr <augustovitor96@gmail.com>
2023-10-31 07:44:38 -0300

    Router sucess

--- pontoperfeito/src/main/java/com/pontoperfeito/pontoperfeito/controller/ClienteControle.java
+++ pontoperfeito/src/main/java/com/pontoperfeito/pontoperfeito/controller/ClienteControle.java
@@ -1,4 +1,6 @@
 package com.pontoperfeito.pontoperfeito.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,34 +11,17 @@ import com.pontoperfeito.pontoperfeito.model.PagamentoModelo;
 
 @RestController
 public class ClienteControle {
-
-    // public class Produto {
-    //     public int id;
-    //     public String nome;
-    //     public double preco;
-
-    //     // Getters e Setters
-    // }
-
-    // Produto ok = new Produto();
-    // ok.id = 1;
-    // ok.nome = 'Breno';
-    // ok.preco = 14.90;
-
-
     @GetMapping("/testes")
     @CrossOrigin(origins = "http://localhost:5173")
-    public String rota(){
+    public ResponseEntity<String> rota() throws JsonProcessingException {
         PagamentoModelo a = new PagamentoModelo();
         ObjectMapper objectMapper = new ObjectMapper();
-        a.setId(1l);
-        a.setPedido_id(1151515l);
+        a.setId(1L);
+        a.setPedido_id(1151515L);
         a.setPaid_at(new Date());
         a.setValor_total(50);
         a.setSituacao(1);
         String pagamentoJson = objectMapper.writeValueAsString(a);
-        // return ResponseEntity.ok(pagamentoJson);
-            return pagamentoJson;
-
-      }
+        return ResponseEntity.ok(pagamentoJson);
+    }
 }
--- pontoperfeito/src/main/java/com/pontoperfeito/pontoperfeito/model/PagamentoModelo.java
+++ pontoperfeito/src/main/java/com/pontoperfeito/pontoperfeito/model/PagamentoModelo.java
@@ -1,10 +1,8 @@
 package com.pontoperfeito.pontoperfeito.model;
 
-import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.Setter;
 import lombok.Getter;
 import java.util.Date;
@@ -13,10 +11,9 @@ import java.util.Date;
 // @Table(name = "pagamentos")
 @Getter
 @Setter
-
 public class PagamentoModelo {
-  // @Id
-  // @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private Long pedido_id; //FK
   private Date paid_at;
